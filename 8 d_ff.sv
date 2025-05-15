e3742d2f6e32bda802aa060a32afd5989b5c8d17
Aname Alahi <anamealahi07@gmail.com>
2025-05-15 21:32:40 +0600

    Update 8 d_ff.sv

--- 8 d_ff.sv
+++ 8 d_ff.sv
@@ -1,10 +1,15 @@
-module top_module(
-	input clk,
-	input [7:0] d,
-	output reg [7:0] q);
-	
-	// Because q is a vector, this creates multiple DFFs.
-	always @(posedge clk)
-		q <= d;
-	
+module top_module (
+    input clk,
+    input reset,            // Synchronous reset
+    input [7:0] d,
+    output reg [7:0] q
+);
+
+    always @(negedge clk) begin
+        if (reset)
+            q <= 8'h34;     // Reset to 0x34 (hexadecimal)
+        else
+            q <= d;
+    end
+
 endmodule
